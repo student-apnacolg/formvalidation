3bcbfcd52d1ae45237a9086da510d2b15c1fef2a
student-apnacolg <nik70655@gmail.com>
2025-06-28 22:19:36 +0530

    except hide all done

--- script.js
+++ script.js
@@ -1,30 +1,71 @@
-let errorMsg = "";
-let missingField = "";
-let submitBtn = document.getElementById("submitInput");
-let messageBox = document.getElementById("message");
-let emailVal = document.getElementById("emailInput").value;
-let phoneVal = document.getElementById("phoneNumberInput").value;
-var passVal = document.getElementById("passInput").value;
-var confirmVal = document.getElementById("confirmInput").value;
+const form = document.getElementById('form');
+const email = document.getElementById('email');
+const phone = document.getElementById('phone');
+const pass = document.getElementById('password');
+const confirm = document.getElementById('confirmPassword');
 
-function isEmail(email) {
-  var regex = /^([a-zA-Z0-9_.+-])+\@(([a-zA-Z0-9-])+\.)+([a-zA-Z0-9]{2,4})+$/;
-  return regex.test(email);
+
+form.addEventListener('submit', e => {
+  e.preventDefault();
+
+  validateInputs();
+});
+
+const validateEmail = (email) => {
+  const regex = /^(([^<>()[\]\\.,;:\s@"]+(\.[^<>()[\]\\.,;:\s@"]+)*)|.(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
+  return regex.test(String(email).toLowerCase());
+};
+
+const setError = (element, mesage) => {
+const inputControl = element.parentElement;
+  const errorDisplay = inputControl.querySelector('.error');
+
+  errorDisplay.innerText = mesage;
+  inputControl.classList.add('error');
+  inputControl.classList.remove('success');
 }
 
-function isNumber(n) {
-  return !isNaN(parseFloat(n)) && isFinite(n);
+const setSuccess = element => {
+  const inputControl = element.parentElement;
+  const errorDisplay = inputControl.querySelector('.error');
+
+  errorDisplay.innerText = '';
+  inputControl.classList.add('success');
+  inputControl.classList.remove('error');
 }
 
-submitBtn.onclick = function() {
-  // if (isEmail(emailVal) == false) {
-  //   errorMsg += "Email id is not Valid."
-  // }
-  // if (isNumber(phoneVal) == false) {
-  //   errorMsg += "Phone Number is not Valid"
-  // }
-  if (passVal != confirmVal) {
-    errorMsg += "Password does not match"
+const validateInputs = () => {
+  const emailValue = email.value.trim();
+  const phoneValue = phone.value.trim();
+  const passValue = pass.value.trim();
+  const confirmValue = confirm.value.trim();
+
+  if (emailValue === '') {
+    setError(email, 'Email is empty')
+  } else if (!validateEmail(emailValue)) {
+    setError(email, 'Email is not valid')
+  } else {
+    setSuccess(email)
   }
-  console.log(errorMsg);
+  if (phoneValue === '') {
+    setError(phone, 'Phone Number is empty')
+  } else if (Number.isInteger(Number(phoneValue)) === false){
+    setError(phone, 'Phone Number is not valid')
+  } else {
+    setSuccess(phone)
+  }
+  if (passValue === '') {
+    setError(pass, 'Password is empty.')
+  } else if(passValue.length < 8){
+    setError(pass, 'Password must be at least 8 characters')
+  } else {
+    setSuccess(pass);
+  }
+  if (confirmValue === '') {
+    setError(confirm, 'Confirm Password is empty.')
+  } else if(confirmValue !== passValue){
+    setError(confirm, 'Password does not match')
+  } else {
+    setSuccess(confirm);
+  }
 }
